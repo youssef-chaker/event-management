5e667aafbde8be190da5764e1fe4ee5a48a816b3
youssef chaker <kikoulolz1youssef@gmail.com>
2021-11-10 10:56:07 +0100

    api gateway

---
+++ src/main/java/com/youssef/pharmacie/apigateway/ApiGatewayConfig.java
@@ -0,0 +1,14 @@
+package com.youssef.pharmacie.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes().route(predicateSpec -> predicateSpec.path("/api/users/**").uri("lb://user-microservice")).build();
+    }
+}
---
+++ src/main/java/com/youssef/pharmacie/apigateway/LoggingFilter.java
@@ -0,0 +1,19 @@
+package com.youssef.pharmacie.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("request path ---> "+exchange.getRequest().getMethod()+" "+exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
