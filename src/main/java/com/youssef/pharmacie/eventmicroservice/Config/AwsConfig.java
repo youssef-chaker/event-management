73dbfc3f46aba0fe04f1b8e2f07105d170043f39
youssef chaker <kikoulolz1youssef@gmail.com>
2021-12-08 17:31:40 +0100

    added upload and retrieve image to/from s3 bucket

--- src/main/java/com/youssef/pharmacie/eventmicroservice/Config/AwsConfig.java
+++ src/main/java/com/youssef/pharmacie/eventmicroservice/Config/AwsConfig.java
@@ -1,8 +1,26 @@
 package com.youssef.pharmacie.eventmicroservice.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Configuration
 public class AwsConfig {
 
+    private Environment env;
+
+    @Autowired
+    public AwsConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public S3Client S3bucket(){
+        return S3Client.builder().region(Region.US_EAST_1).build();
+    }
 }
--- src/main/java/com/youssef/pharmacie/eventmicroservice/controller/EventController.java
+++ src/main/java/com/youssef/pharmacie/eventmicroservice/controller/EventController.java
@@ -11,13 +11,24 @@ import com.youssef.pharmacie.eventmicroservice.entity.Event;
 import com.youssef.pharmacie.eventmicroservice.entity.Tag;
 import com.youssef.pharmacie.eventmicroservice.entity.User;
 import lombok.extern.log4j.Log4j2;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -29,15 +40,19 @@ public class EventController {
 
     private final EventRepo eventRepo;
     private final UsersProxy usersProxy;
+    private final S3Client s3;
+    private final Environment env;
 
     @Autowired
-    public EventController(EventRepo eventRepo, UsersProxy usersProxy) {
+    public EventController(EventRepo eventRepo, UsersProxy usersProxy, S3Client s3, Environment env) {
         this.eventRepo = eventRepo;
         this.usersProxy = usersProxy;
+        this.s3 = s3;
+        this.env = env;
     }
 
     @PostMapping
-    public ResponseEntity insertEvent(@RequestBody EventDto eventDto, @AuthenticationPrincipal Jwt jwt){
+    public ResponseEntity<?> insertEvent(@RequestBody EventDto eventDto, @AuthenticationPrincipal Jwt jwt){
         try {
             System.out.println(eventDto);
             Geometry geometry = new WKTReader().read(eventDto.getPoint());
@@ -59,23 +74,6 @@ public class EventController {
             return ResponseEntity.badRequest().build();
         }
     }
-
-
-//    @GetMapping
-//    public ResponseEntity<List<Event>> getAllEvents(@RequestBody LongLat longLat, @RequestParam String orderBy,@RequestParam int order,@RequestParam int limit,@RequestParam int offset){
-//        List<Event> events ;
-//        if(orderBy != null){
-//            events = eventRepo.getEvents();
-//        }else{
-//            events = eventRepo.getEvents();
-//        }
-//        return ResponseEntity.ok(events);
-//    }
-
-//    @GetMapping("/test")
-//    public String dsfsd(){
-//        return GeometryFactory.
-//    }
 
     @GetMapping
     public ResponseEntity<List<Event>> getAllEvents(@RequestParam(required = false) Double longitude,@RequestParam(required = false) Double latitude){
@@ -102,5 +100,29 @@ public class EventController {
     public void subscribe(@PathVariable long id,@AuthenticationPrincipal Jwt jwt){
         User user = usersProxy.getUserById(jwt.getClaims().get("sub").toString());
         eventRepo.subscribe(user,id);
+    }
+
+    @PostMapping("/{id}/image")
+    public ResponseEntity<?> addImage(@RequestParam("image") MultipartFile file,@PathVariable("id") long id){
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(env.getProperty("s3.bucket"))
+                .key(id+".png")
+                .build();
+        try {
+            s3.putObject(objectRequest, software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/image")
+    public ResponseEntity<?> getImage(@PathVariable("id") long id){
+        GetObjectRequest objectRequest = GetObjectRequest.builder()
+                .bucket(env.getProperty("s3.bucket"))
+                .key(id+".png")
+                .build();
+        var image = s3.getObject(objectRequest, ResponseTransformer.toBytes());
+        return ResponseEntity.status(HttpStatus.OK).header("Content-type", MediaType.IMAGE_PNG_VALUE).body(image.asByteArray());
     }
 }
